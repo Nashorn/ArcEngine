a6fb6cd36a3704fd605f99e4ca2661b279fcac75
Jason E. Smith <jaysmith024@gmail.com>
2020-06-16 07:44:39 -0400

    initial barebones commit

---
+++ -buildconfig.js
@@ -0,0 +1,37 @@
+BUILDCONFIG = {
+    // LoadPaths:[
+    //     "/", 
+    //     "src", 
+    //     "libs"
+    // ],
+    
+    Input : "src/applications/NotificationsApp/index.js",
+    
+    Output : {
+        SourcePath:"src/applications/NotificationsApp/index.src.js",
+        CompressedPath: "src/applications/NotificationsApp/index.min.js"
+    },
+
+    LoadsAsync : true,
+
+    // Output : {
+    //     SourcePath:"src/applications/InlineTest/index.src.js",
+    //     CompressedPath: "src/applications/InlineTest/index.min.js"
+    // },
+
+    // Output : {
+    //  SourcePath:"node_modules/od-cocoon/framework.src.js",
+    //  CompressedPath: "node_modules/od-cocoon/framework.min.js"
+    // },
+    
+    Verbos:true,
+
+    Prefabs : {
+        Enabled : true,
+        Components : [
+            "applications.NotificationsApp",
+            "core.ui.NotificationsToggleSwitch"
+        ]
+    }
+};
+module.exports = BUILDCONFIG;
